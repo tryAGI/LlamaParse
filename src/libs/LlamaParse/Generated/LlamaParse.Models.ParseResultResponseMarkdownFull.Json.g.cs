a5b203639c26c14e4cda360e9c23b533c8e90994
Konstantin S. <havendv@gmail.com>
2026-06-17 15:03:35 +0400

    feat: Updated OpenAPI spec (#65)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/LlamaParse/Generated/LlamaParse.Models.ParseResultResponseMarkdownFull.Json.g.cs
+++ src/libs/LlamaParse/Generated/LlamaParse.Models.ParseResultResponseMarkdownFull.Json.g.cs
@@ -17,6 +17,14 @@ namespace LlamaParse
         }
 
         /// <summary>
+        /// Serializes the current instance to a JSON string using the generated default JsonSerializerContext.
+        /// </summary>
+        public string ToJson()
+        {
+            return ToJson(global::LlamaParse.SourceGenerationContext.Default);
+        }
+
+        /// <summary>
         /// Serializes the current instance to a JSON string using the provided JsonSerializerOptions.
         /// </summary>
 #if NET8_0_OR_GREATER
@@ -26,6 +34,11 @@ namespace LlamaParse
         public string ToJson(
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
+            if (jsonSerializerOptions is null)
+            {
+                return ToJson(global::LlamaParse.SourceGenerationContext.Default);
+            }
+
             return global::System.Text.Json.JsonSerializer.Serialize(
                 this,
                 jsonSerializerOptions);
@@ -45,6 +58,17 @@ namespace LlamaParse
         }
 
         /// <summary>
+        /// Deserializes a JSON string using the generated default JsonSerializerContext.
+        /// </summary>
+        public static global::LlamaParse.ParseResultResponseMarkdownFull? FromJson(
+            string json)
+        {
+            return FromJson(
+                json,
+                global::LlamaParse.SourceGenerationContext.Default);
+        }
+
+        /// <summary>
         /// Deserializes a JSON string using the provided JsonSerializerOptions.
         /// </summary>
 #if NET8_0_OR_GREATER
@@ -55,6 +79,13 @@ namespace LlamaParse
             string json,
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
+            if (jsonSerializerOptions is null)
+            {
+                return FromJson(
+                    json,
+                    global::LlamaParse.SourceGenerationContext.Default);
+            }
+
             return global::System.Text.Json.JsonSerializer.Deserialize<global::LlamaParse.ParseResultResponseMarkdownFull>(
                 json,
                 jsonSerializerOptions);
@@ -74,6 +105,17 @@ namespace LlamaParse
         }
 
         /// <summary>
+        /// Deserializes a JSON stream using the generated default JsonSerializerContext.
+        /// </summary>
+        public static global::System.Threading.Tasks.ValueTask<global::LlamaParse.ParseResultResponseMarkdownFull?> FromJsonStreamAsync(
+            global::System.IO.Stream jsonStream)
+        {
+            return FromJsonStreamAsync(
+                jsonStream,
+                global::LlamaParse.SourceGenerationContext.Default);
+        }
+
+        /// <summary>
         /// Deserializes a JSON stream using the provided JsonSerializerOptions.
         /// </summary>
 #if NET8_0_OR_GREATER
@@ -84,6 +126,13 @@ namespace LlamaParse
             global::System.IO.Stream jsonStream,
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
+            if (jsonSerializerOptions is null)
+            {
+                return FromJsonStreamAsync(
+                    jsonStream,
+                    global::LlamaParse.SourceGenerationContext.Default);
+            }
+
             return global::System.Text.Json.JsonSerializer.DeserializeAsync<global::LlamaParse.ParseResultResponseMarkdownFull?>(
                 jsonStream,
                 jsonSerializerOptions);
