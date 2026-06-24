61c7f84cc2c8538eeaed8afdc257bf2ff4bd9949
Konstantin S. <havendv@gmail.com>
2026-06-24 13:37:30 +0400

    feat: Updated OpenAPI spec (#69)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/LlamaParse/Generated/LlamaParse.Models.AutoModeParsingConfVersion.g.cs
+++ src/libs/LlamaParse/Generated/LlamaParse.Models.AutoModeParsingConfVersion.g.cs
@@ -15,11 +15,7 @@ namespace LlamaParse
         /// <summary>
         /// 
         /// </summary>
-        x20260611,
-        /// <summary>
-        /// 
-        /// </summary>
-        x20260617,
+        x20260618,
         /// <summary>
         /// 
         /// </summary>
@@ -39,8 +35,7 @@ namespace LlamaParse
             return value switch
             {
                 AutoModeParsingConfVersion.x20251211 => "2025-12-11",
-                AutoModeParsingConfVersion.x20260611 => "2026-06-11",
-                AutoModeParsingConfVersion.x20260617 => "2026-06-17",
+                AutoModeParsingConfVersion.x20260618 => "2026-06-18",
                 AutoModeParsingConfVersion.Latest => "latest",
                 _ => throw new global::System.ArgumentOutOfRangeException(nameof(value), value, null),
             };
@@ -53,8 +48,7 @@ namespace LlamaParse
             return value switch
             {
                 "2025-12-11" => AutoModeParsingConfVersion.x20251211,
-                "2026-06-11" => AutoModeParsingConfVersion.x20260611,
-                "2026-06-17" => AutoModeParsingConfVersion.x20260617,
+                "2026-06-18" => AutoModeParsingConfVersion.x20260618,
                 "latest" => AutoModeParsingConfVersion.Latest,
                 _ => null,
             };
